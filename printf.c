caae1742b084c141990492de2d19f70a2284c3db
genyarko <115112347+genyarko@users.noreply.github.com>
2023-01-23 14:44:12 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,46 +1,51 @@
 #include <stdarg.h>
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
-/**
-* _printf - function that produces output according to a format
-* @format: character string
-*
-* Return: the number of characters printed (excluding the null byte)
-*/
 int _printf(const char *format, ...)
 {
-va_list args;
-int i, count = 0;
-va_start(args, format);
-if (!format || !*format)
-return (-1);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%' && format[i + 1] == 'c')
-{
-printf("%c", va_arg(args, int));
-count++;
-i++;
-}
-else if (format[i] == '%' && format[i + 1] == 's')
-{
-printf("%s", va_arg(args, char *));
-count += strlen(va_arg(args, char *));
-i++;
-}
-else if (format[i] == '%' && format[i + 1] == '%')
-{
-printf("%%");
-count++;
-i++;
-}
-else
-{
-printf("%c", format[i]);
-count++;
-}
-}
-va_end(args);
-return (count);
+    va_list args;
+    int i = 0;
+    int count = 0;
+
+    va_start(args, format);
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == '%')
+            {
+                putchar('%');
+                count++;
+                i += 2;
+            }
+            else if (format[i + 1] == 'c')
+            {
+                putchar(va_arg(args, int));
+                count++;
+                i += 2;
+            }
+            else if (format[i + 1] == 's')
+            {
+                char *s = va_arg(args, char *);
+                int j = 0;
+
+                while (s[j] != '\0')
+                {
+                    putchar(s[j]);
+                    j++;
+                    count++;
+                }
+                i += 2;
+            }
+        }
+        else
+        {
+            putchar(format[i]);
+            count++;
+            i++;
+        }
+    }
+    va_end(args);
+
+    return (count);
 }
