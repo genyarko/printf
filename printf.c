fe3982fee64c20ecd19a049977e41cd3a7f67571
genyarko <115112347+genyarko@users.noreply.github.com>
2023-01-24 11:44:44 +0100

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,44 +1,38 @@
-
 #include <stdarg.h>
 #include <stdio.h>
 int _printf(const char *format, ...)
 {
-	int i = 0, leng = 0, count = 0;
-	va_list args;
-	char *str;
-
-	va_start(args, format);
-	if (!format || (*format == '%' && strlen(format) == 1))
-		return (-1);
-	while (format[i] != '\0')
-	{
-		if (format[i - 1] != '%' && format[i] != '%')
-			putchar(format[i]);
-		else if (format[i - 1] == '%')
-		{	count = count + 2;
-			switch (format[i])
-			{   case 'c':
-					leng++;
-					putchar((char)va_arg(args, int));
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					leng += _put(str);
-					break;
-				case 'i':
-				case 'd':
-					leng += print_number(va_arg(args, int));
-					break;
-				case 'b':
-					leng += _bin(va_arg(args, int));
-					break;
-				default:
-					leng +=	_other_print(format[i], format[i - 1], format[i + 1]);
-					break;
-			}
-		}
-		i++;
-	}
-	va_end(args);
-	return (i + leng - count);
+int result = 0;
+va_list args;
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+result += printf("%c", va_arg(args, int));
+break;
+case 's':
+result += printf("%s", va_arg(args, char *));
+break;
+case 'd':
+case 'i':
+result += printf("%d", va_arg(args, int));
+break;
+case '%':
+result += printf("%%");
+break;
+}
+}
+else
+{
+result += printf("%c", *format);
+}
+format++;
+}
+va_end(args);
+return result;
 }
