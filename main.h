cce3d96b91cd523a2774a88dd4793339a119d321
genyarko <115112347+genyarko@users.noreply.github.com>
2023-01-24 12:04:49 +0100

    Update main.h

--- main.h
+++ main.h
@@ -7,14 +7,4 @@ int _printf(const char *format, ...);
 int putchar();
 int _putchar();
 void print_strings(const char *separator, const unsigned int n, ...);
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list list);
-int print_string(va_list list);
-int print_percent(va_list list);
-int print_int(va_list list);
-int print_unsigned(va_list list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_HEX(va_list list);
 #endif
